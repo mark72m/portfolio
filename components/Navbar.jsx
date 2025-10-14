b6a48f212ebda989b098eda46a6e330ce5fc400d
markm <markmutisya72@gmail.com>
2025-10-13 23:28:17 -0700

    1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,9 @@
+import { useEffect } from "react"
 
-export const Navbar = () => {
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    })
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg ">
             <div className="max-w-5xl mx-auto px-4">
@@ -7,18 +11,20 @@ export const Navbar = () => {
                     <a href="#home" className="font-mono text-xl font-bold text-white">
                         {""}
                         market<span className="text-blue-500">.tech</span>{""}
-                        </a>
+                    </a>
 
-                        <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
-                            &#9776;
-                        </div>
+                    <div
+                        className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+                        onClick={() => setMenuOpen((prev) => !prev)}>
+                        &#9776;
+                    </div>
 
-                        <div className="hidden md:flex items-center space-x-8">
-                            <a href="#home" className="tex-gray-300 hove:text-white transition-colors">{""}Home{""}</a>
-                            <a href="#about" className="tex-gray-300 hove:text-white transition-colors">{""}About{""}</a>
-                            <a href="#projects" className="tex-gray-300 hove:text-white transition-colors">{""}Projects{""}</a>
-                            <a href="#contact" className="tex-gray-300 hove:text-white transition-colors">{""}Contact{""}</a>
-                        </div>
+                    <div className="hidden md:flex items-center space-x-8">
+                        <a href="#home" className="tex-gray-300 hove:text-white transition-colors">{""}Home{""}</a>
+                        <a href="#about" className="tex-gray-300 hove:text-white transition-colors">{""}About{""}</a>
+                        <a href="#projects" className="tex-gray-300 hove:text-white transition-colors">{""}Projects{""}</a>
+                        <a href="#contact" className="tex-gray-300 hove:text-white transition-colors">{""}Contact{""}</a>
+                    </div>
                 </div>
             </div>
         </nav>
